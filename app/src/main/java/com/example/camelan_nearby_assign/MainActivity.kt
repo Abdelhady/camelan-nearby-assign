9050cb700446fe8269101a62df31139cdbe60446
Abdel Hady <obad85@gmail.com>
2020-07-20 20:42:06 +0200

    Managing Location permission

--- app/src/main/java/com/example/camelan_nearby_assign/MainActivity.kt
+++ app/src/main/java/com/example/camelan_nearby_assign/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.camelan_nearby_assign
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.observe
 import com.example.camelan_nearby_assign.databinding.ActivityMainBinding
@@ -12,19 +18,56 @@ class MainActivity : AppCompatActivity() {
 
     private val mainViewModel by viewModels<MainViewModel>()
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                mainViewModel.hasPermission.value = true
+            } else {
+                Toast.makeText(
+                    this,
+                    R.string.location_permission_declined_message,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewmodel = mainViewModel
         binding.lifecycleOwner = this
-        permissionButton.setOnClickListener {
-            // TODO ask for permission first before showing the fragment
-            mainViewModel.hasPermission.value = true
-        }
+        checkLocationPermission()
         mainViewModel.hasPermission.observe(this) {
             if (it) {
                 loadNearbyFragment()
+            }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // Permissions already granted while installing from Google Play
+            mainViewModel.hasPermission.value = true
+            return
+        }
+        // https://developer.android.com/training/permissions/requesting#allow-system-manage-request-code
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                mainViewModel.hasPermission.value = true
+            }
+            else -> {
+                permissionButton.setOnClickListener {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                }
             }
         }
     }
