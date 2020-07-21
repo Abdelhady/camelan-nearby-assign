07cb26d23be966f166fef574a1dc53bb1eca2397
Abdel Hady <obad85@gmail.com>
2020-07-21 20:33:44 +0200

    Getting location updates

--- app/src/main/java/com/example/camelan_nearby_assign/NearbyLocationsFragment.kt
+++ app/src/main/java/com/example/camelan_nearby_assign/NearbyLocationsFragment.kt
@@ -1,20 +1,31 @@
 package com.example.camelan_nearby_assign
 
-import androidx.lifecycle.ViewModelProviders
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.location.*
+import timber.log.Timber
 
 class NearbyLocationsFragment : Fragment() {
+
+    private var requestingLocationUpdates = true // TODO update this as per user's preference
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var currentLocation: Location
 
     companion object {
         fun newInstance() = NearbyLocationsFragment()
     }
 
-//    private lateinit var viewModel: NearbyLocationsViewModel
     val viewModel by activityViewModels<NearbyLocationsViewModel>()
 
     override fun onCreateView(
@@ -27,6 +38,82 @@ class NearbyLocationsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // TODO: Use the ViewModel
+        fetchLastKnownLocation()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                Timber.d("locations found are ${locationResult.locations.size} locations")
+                if (locationResult.locations.size == 0) return
+                currentLocation = locationResult.locations[0]
+                Timber.d("current location details .. Longitude: ${currentLocation.longitude}, Latitude: ${currentLocation.latitude}")
+                // TODO refresh nearby locations list here
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // TODO if last known location was null, we still need to get location updates at least once!
+        if (requestingLocationUpdates) startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // This shouldn't be reached, because activity is responsible for requesting the permission
+            Timber.d("location permission not exists")
+            return
+        }
+        // TODO extract interval/fastestInterval as separate constants to be used here & in the activity
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun fetchLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // This shouldn't be reached, because activity is responsible for requesting the permission
+            Timber.d("location permission not exists")
+            return
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    lastLocation =
+                        location // TODO should be added as a live data in the view model?
+                    Timber.d("last location details .. Longitude: ${location.longitude}, Latitude: ${location.latitude}")
+                } else {
+                    Timber.d("location is null")
+                    // This means that user didn't use any app that asks for location (like maps)
+                    // since last time the user enabled device's location setting
+                }
+            }
     }
 
 }
